87cb6a911079553807a8aaf4143018928b380e0c
Skrepy <155234406+Skrepy0@users.noreply.github.com>
2026-08-06 17:16:03 +0800

    chore: 修改CORS 配置

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=['*'],
+    allow_methods=['GET', 'POST', 'OPTIONS'],
     allow_headers=['*'],
 )
 
