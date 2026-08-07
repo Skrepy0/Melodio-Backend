a1dba62b9971a28c6f664dc601b8d98966d6bb63
Skrepy <155234406+Skrepy0@users.noreply.github.com>
2026-08-07 20:48:04 +0800

    feat: 完善Music Client

--- app/schemas/client.py
+++ app/schemas/client.py
@@ -4,30 +4,24 @@ from enum import StrEnum
 class MusicClient(StrEnum):
     Bilibili = 'BilibiliMusicClient'
     Bodian = 'BodianMusicClient'
-    FiveSing = 'FiveSingMusicClient'
     Kugou = 'KugouMusicClient'
     Kuwo = 'KuwoMusicClient'
     Migu = 'MiguMusicClient'
-    MOOV = 'MOOVMusicClient'
     Netease = 'NeteaseMusicClient'
     Qianqian = 'QianqianMusicClient'
     QQ = 'QQMusicClient'
-    Soda = 'SodaMusicClient'
-    StreetVoice = 'StreetVoiceMusicClient'
-
-    # Apple = "AppleMusicClient"
-    # Deezer = "DeezerMusicClient"
-    # FMA = "FMAMusicClient"
-    # Jamendo = "JamendoMusicClient"
-    # Joox = "JooxMusicClient"
-    # JioSaavn = "JioSavnMusicClient"
-    # OpenGameArt = "OpenGameArtMusicClient"
-    # Qobuz = "QobuzMusicClient"
-    # SoundCloud = "SoundCloudMusicClient"
-    # Spotify = "SpotifyMusicClient"
-    # Suno = "SunoMusicClient"
-    # TIDAL = "TIDALMusicClient"
-    # YouTube = "YouTubeMusicClient"
+    Apple = 'AppleMusicClient'
+    Joox = 'JooxMusicClient'
+    Qobuz = 'QobuzMusicClient'
+    Suno = 'SunoMusicClient'
+    MyFreeMP3 = 'MyFreeMP3MusicClient'
+    TuneHub = 'TuneHubMusicClient'
+    XiaoBai = 'XiaoBaiMusicClient'
+    Fangpi = 'FangpiMusicClient'
+    Gequbao = 'GequbaoMusicClient'
+    Gequhai = 'GequhaiMusicClient'
+    Mitu = 'MituMusicClient'
+    Zhuolin = 'ZhuolinMusicClient'
 
     @staticmethod
     def is_valid_music_client(value: str) -> bool:
